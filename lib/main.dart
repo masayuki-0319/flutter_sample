999e45e09747a65b78ff6e44540cb9baae23451b
将之大西 <omccello@gmail.com>
2021-07-26 23:37:25 +0900

    step18

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); // 引数のWidgetが画面いっぱいに表示される
 
-class RandomWordsState extends State<RandomWords> {
-  Widget _buildSuggestions() {
-    final _wordPairs = <String>[];
-
-    return ListView.builder(
-      itemBuilder: (context, i) { // itemBuilderで一行ごとに処理が呼ばれる
-        if (i.isOdd) return Divider();  // 奇数行には水平線を表示
-
-        final index = i ~/ 2;  // ~/は結果が整数の割り算
-        if (index >= _wordPairs.length) {  // 行数がリストの要素数を越えれば
-          _wordPairs.addAll(["a", "b", "c", "d"].take(2));  // 単語のペアを10個追加
-        }
-
-        return _buildRow(_wordPairs[index]);
-      }
-    );
-  }
-
-  // 単語のペアから、形式を整えた行のWidgetを作るメソッド
-  Widget _buildRow(String pair) {
-    return ListTile(
-      title: Text(pair),
-    );
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Live!人工知能"),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-}
-
-// StatefulなWidgetのクラスは、StatefulWidgetを継承
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-// 最初に表示するWidgetのクラス
-class MyApp extends StatelessWidget {  // StatelessWidgetを継承
-  @override
-  Widget build(BuildContext context) {  //buildメソッドでUIを作成
-    return MaterialApp(  // マテリアルデザインのアプリ
-      title: "My Simple App",  // アプリのタイトル
-      home: RandomWords()
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("My Simple App"),
+            ),
+            body: Stack(  // 重ねて表示
+              children: <Widget>[
+                Container(
+                    width: 800,
+                    height: 600,
+                    color:Colors.red
+                ),
+                Container(
+                  width: 120,
+                  height: 120,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.blue,
+                )
+              ],
+            )
+        )
     );
   }
 }
