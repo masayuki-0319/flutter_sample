1053c55404fc78449c622f18234d96a00c25e4c7
将之大西 <omccello@gmail.com>
2021-07-27 00:48:22 +0900

    step19

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,57 @@
+/* 3-4. スライダー */
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp()); // 引数のWidgetが画面いっぱいに表示される
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("My Simple App"),
-            ),
-            body: Stack(  // 重ねて表示
-              children: <Widget>[
-                Container(
-                    width: 800,
-                    height: 600,
-                    color:Colors.red
-                ),
-                Container(
-                  width: 120,
-                  height: 120,
-                  color: Colors.green,
-                ),
-                Container(
-                  width: 50,
-                  height: 50,
-                  color: Colors.blue,
-                )
-              ],
+      title: "My Simple App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Live!人工知能"),
+        ),
+        body: Center(
+          child: MyForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyForm extends StatefulWidget {
+  @override
+  _MyFormState createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  double _value = 0.0;
+
+  void _changeSlider(double e) {
+    setState(() {  // 状態を保持する変数を変更する処理は、setState内に記述する
+      _value = e;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(child:Text("値：$_value")),
+            Slider(  // スライダー
+              label: '$_value',
+              min: 0,
+              max: 100,
+              value: _value,
+              activeColor: Colors.orange,
+              inactiveColor: Colors.blueAccent,
+              divisions: 5,
+              onChanged: _changeSlider,
             )
+          ],
         )
     );
   }
