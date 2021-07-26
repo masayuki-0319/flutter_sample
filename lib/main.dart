3268d5fdaaa6e59ddfeaf3fd85e424d64a99c480
将之大西 <omccello@gmail.com>
2021-07-26 23:22:19 +0900

    step16

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+/* 1-2. 外部パッケージの導入 */
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp()); // 引数のWidgetが画面いっぱいに表示される
+
+// 最初に表示するWidgetのクラス
+class MyApp extends StatelessWidget {
+  // StatelessWidgetを継承
+  @override
+  Widget build(BuildContext context) {
+    //buildメソッドでUIを作成
+    final wordPair = "hello" + "_" + "world";
+    return MaterialApp(
+      // マテリアルデザインのアプリ
+      title: "My Simple App", // アプリのタイトル
+      home: Scaffold(
+        // マテリアルデザインの土台
+        appBar: AppBar(
+          // ヘッダーに表示するアプリケーションバー
+          title: Text("Live!人工知能！！"), // タイトルを表示
+        ),
+        body: Center(
+          // 中央に配置
+          child: Text(wordPair.toUpperCase()), // UpperCamelCaseで表示
+        ),
+      ),
+    );
+  }
+}
